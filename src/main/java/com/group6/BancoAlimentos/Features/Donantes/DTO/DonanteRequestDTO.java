0615c19e69c246f7f8af0c8ad887954001090aad
valentindona <valentindona3@gmail.com>
2026-04-25 23:52:51 -0300

    DTO DONANTES
    creo dto de request y response para donante

---
+++ src/main/java/com/group6/BancoAlimentos/Features/Donantes/DTO/DonanteRequestDTO.java
@@ -0,0 +1,19 @@
+package com.group6.BancoAlimentos.Features.Donantes.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class DonanteRequestDTO {
+
+    private String razon_social;
+    private Long cuit;
+    private String telefono;
+    private String email;
+    private String direccion;
+}
