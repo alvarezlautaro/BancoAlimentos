886cbac8c92a76bdca6648966b309eb0f6cd381d
valentindona <valentindona3@gmail.com>
2026-04-25 23:53:43 -0300

    EXPECTION CUIT DUPLICADO
    creo la expection para manejar cuits duplicados

---
+++ src/main/java/com/group6/BancoAlimentos/Features/Donantes/expections/CuitDuplicadoExpections.java
@@ -0,0 +1,7 @@
+package com.group6.BancoAlimentos.Features.Donantes.expections;
+
+public class CuitDuplicadoExpections extends RuntimeException {
+    public CuitDuplicadoExpections(String message) {
+        super(message);
+    }
+}
