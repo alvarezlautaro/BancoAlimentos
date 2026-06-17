ac2cd85cb540cbbc3abe036289c69c28647b2816
valentindona <valentindona3@gmail.com>
2026-06-17 15:13:37 -0300

    extense del commit anterior

--- src/main/java/com/group6/BancoAlimentos/Features/Donantes/controller/DonanteController.java
+++ src/main/java/com/group6/BancoAlimentos/Features/Donantes/controller/DonanteController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,24 +20,27 @@ public class DonanteController {
 
     private final IDonanteService donanteService;
 
-
+    @PreAuthorize("hasAuthority('EMPRESA_VER')")
     @GetMapping
     public ResponseEntity<List<DonanteResponseDTO>> findAllDonantes() {
         return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(donanteService.findAll());
 
     }
 
+    @PreAuthorize("hasAuthority('EMPRESA_VER')")
     @GetMapping("/{id}")
     public ResponseEntity<DonanteResponseDTO> findDonanteById(@PathVariable Long id) {
         DonanteResponseDTO responseDTO = donanteService.findById(id).orElseThrow(() -> new RuntimeException("El donante no existe"));
         return ResponseEntity.ok(responseDTO);
     }
 
+    @PreAuthorize("hasAuthority('EMPRESA_CREAR')")
     @PostMapping
     public ResponseEntity<DonanteResponseDTO> create(@Valid @RequestBody DonanteRequestDTO donante){
         return ResponseEntity.status(HttpStatus.CREATED).body(donanteService.save(donante));
     }
 
+    @PreAuthorize("hasAuthority('EMPRESA_ACTUALIZAR')")
     @PutMapping("/{id}")
     public ResponseEntity<DonanteResponseDTO> updateDonante(@PathVariable Long id ,
                                                               @RequestBody @Valid DonanteRequestDTO donanteRequestDTO){
@@ -44,6 +48,7 @@ public class DonanteController {
 
     }
 
+    @PreAuthorize("hasAuthority('EMPRESA_ELIMINAR')")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteDonanteById(@PathVariable Long id){
         donanteService.delete(id);
