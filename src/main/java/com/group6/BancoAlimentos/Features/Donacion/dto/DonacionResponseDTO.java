c74765375f39e6350c088370521bb4d3d5e1e0b0
valentindona <valentindona3@gmail.com>
2026-06-19 11:27:22 -0300

    Agregado de campos de itemDonacion a Donacion para mejor muestra de datos

--- src/main/java/com/group6/BancoAlimentos/Features/Donacion/dto/DonacionResponseDTO.java
+++ src/main/java/com/group6/BancoAlimentos/Features/Donacion/dto/DonacionResponseDTO.java
@@ -1,9 +1,11 @@
 package com.group6.BancoAlimentos.Features.Donacion.dto;
 
 import com.group6.BancoAlimentos.Features.Donacion.model.EstadoDonacion;
+import com.group6.BancoAlimentos.Features.ItemDonacion.dto.ItemDonacionResponseDTO;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 
 @Getter
@@ -17,4 +19,5 @@ public class DonacionResponseDTO {
     private EstadoDonacion estado;
     private int nroRemitoProveedor;
     private String observaciones;
+    private List<ItemDonacionResponseDTO> items;
 }
